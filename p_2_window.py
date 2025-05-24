34b0f86a8718bc9393d00474e3133bbaafd5b131
asrahim88 <asrahim162@gmail.com>
2025-05-24 21:31:02 +0600

    second program of tkiter

---
+++ p_2_window.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+window  = Tk()
+window.title("my apps")
+window.geometry("400x300+250+250")
+window.minsize(300, 200)
+window.maxsize(500, 400)
+
+window.configure(bg= 'black')
+
+
+window.mainloop()
